73b32d4b125e20e2007b6af001cff813ffcf6d3a
Richard Hvizdoš <richard.hvizdos@outlook.sk>
2023-06-29 12:16:31 +0200

    added lastExecutionForIds to dependency arr

--- frontend/src/components/graph-visualisation.js
+++ frontend/src/components/graph-visualisation.js
@@ -174,7 +174,7 @@ export function GraphVisualisationFromIds({graphIds}){
             ignore = true;
         }
 
-    }, [graphIds, baseUrl]);
+    }, [graphIds, baseUrl, lastExecutionForIds]);
     
     
 
